6143ce8b51f86fed1fc82c7b001e490a0da5d6f2
dojo2022-hosono <103239191+dojo2022-hosono@users.noreply.github.com>
2022-06-16 14:55:35 +0900

    Post beans更新

--- src/src/model/post/Post.java
+++ src/src/model/post/Post.java
@@ -2,6 +2,68 @@ package model.post;
 
 import java.io.Serializable;
 
+import org.joda.time.DateTime;
+
 public class Post implements Serializable {
+	private int post_id;
+	private int user_id;
+	private String title;
+	private String body;
+	private int heart;
+	private int useful;
+	private int week;
+	private DateTime created_at;
+
+
+	public int getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public int getHeart() {
+		return heart;
+	}
+	public void setHeart(int heart) {
+		this.heart = heart;
+	}
+	public int getUseful() {
+		return useful;
+	}
+	public void setUseful(int useful) {
+		this.useful = useful;
+	}
+	public int getWeek() {
+		return week;
+	}
+	public void setWeek(int week) {
+		this.week = week;
+	}
+	public DateTime getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(DateTime created_at) {
+		this.created_at = created_at;
+	}
+
+
 
 }
