e854c70fe0f0121333ad03fac9665fc3da97e4da
dojo2022-inomata <103239582+dojo2022-inomata@users.noreply.github.com>
2022-06-16 15:45:46 +0900

    columnとcolumn_genle追加

--- src/src/model/column/Column.java
+++ src/src/model/column/Column.java
@@ -3,5 +3,34 @@ package model.column;
 import java.io.Serializable;
 
 public class Column implements Serializable {
+	private int column_id;
+	private String title;
+	private String body;
+	private int genre_id;
+	public int getColumn_id() {
+		return column_id;
+	}
+	public void setColumn_id(int column_id) {
+		this.column_id = column_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public int getGenre_id() {
+		return genre_id;
+	}
+	public void setGenre_id(int genre_id) {
+		this.genre_id = genre_id;
+	}
+
 
 }
