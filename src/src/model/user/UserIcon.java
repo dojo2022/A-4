455597e45890d47bd51df382f9501bb2f95ea0a5
dojo2022-kobayashi <103238349+dojo2022-kobayashi@users.noreply.github.com>
2022-06-16 16:29:05 +0900

    ChildcareQuestLabel、LaborPain、UserIcon、WeeklyReport追加 LaborPainはインデントのみ調整

--- src/src/model/user/UserIcon.java
+++ src/src/model/user/UserIcon.java
@@ -4,4 +4,26 @@ import java.io.Serializable;
 
 public class UserIcon implements Serializable {
 
+	private int image_id;
+	private String person;
+	private String image;
+
+	public int getImage_id() {
+		return image_id;
+	}
+	public void setImage_id(int image_id) {
+		this.image_id = image_id;
+	}
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
 }
