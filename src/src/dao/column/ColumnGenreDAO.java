2101e45b18bf111279aa0a94e796d7ce27b2d91a
dojo2022-inomata <103239582+dojo2022-inomata@users.noreply.github.com>
2022-06-21 15:36:27 +0900

    コラム

--- src/src/dao/column/ColumnGenreDAO.java
+++ src/src/dao/column/ColumnGenreDAO.java
@@ -1,5 +1,70 @@
 package dao.column;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.column.ColumnGenre;
+
 public class ColumnGenreDAO {
+
+	public ArrayList<ColumnGenre> select() {
+		Connection conn = null;
+		ArrayList<ColumnGenre> columnList = new ArrayList<ColumnGenre>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			// SQL文を準備する
+			String sql = "SELECT * from column_genre";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+
+
+			// SQL文を実行し、結果表を取得する　りざるとせっとにあてはまる項目を表のまま入る　エコバック　resultsetなんでも入るボックス　表が入っている
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする　表だったものをarray１、２、３として詰め替えている　他のファイルでも読み込めるようになる
+			while (rs.next()) {
+				ColumnGenre column = new ColumnGenre();
+				column.setColumn_genre_id(rs.getInt("column_genre_id"));
+				column.setName(rs.getString("name"));
+				column.setImage(rs.getString("image"));
+
+				columnList.add(column);
+			}
+			System.out.println(columnList.size()+"←とってきた数");
+		}
+		catch (SQLException e) {   //エラーが出たらキャッチ
+			e.printStackTrace();
+			columnList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			columnList = null;
+		}
+		finally {  //conn白紙の地図に戻す
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					columnList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return columnList;
+	}
 
 }
--- src/src/servlet/column/ColumnArticleServlet.java
+++ src/src/servlet/column/ColumnArticleServlet.java
@@ -1,6 +1,7 @@
 package servlet.column;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +20,7 @@ public class ColumnArticleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at:").append(request.getContextPath());
 	}
 
 }
