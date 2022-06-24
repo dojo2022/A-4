13b35ca85ceb851f5d359ca2df92a943f7887915
dojo2022-kobayashi <103238349+dojo2022-kobayashi@users.noreply.github.com>
2022-06-24 12:04:09 +0900

    プロフィール編集のjsp,更新、DAOテスト作成途中

--- src/src/daotest/user/UserProfileDAOTest.java
+++ src/src/daotest/user/UserProfileDAOTest.java
@@ -1,66 +1,20 @@
 package daotest.user;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import dao.user.UserDAO;
 
 public class UserProfileDAOTest {
 
 	public static void main(String[] args) {
-		System.out.println("---------- updateUserのテスト ----------");
-		//getLaborPainTest();	// ユーザーが見つかる場合のテスト
+		System.out.println("---------- getUserProfileDAOのテスト ----------");
+		getUserProfileDAOTest();
 
 	}
 
 
-	public boolean updateUser(String name, String nickname, String post_code, String address, String tel, Date birthday, String gender, Date due_date) {
-		Connection conn = null;
-		boolean result = false;
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
+	public static void getUserProfileDAOTest() {
+		UserDAO uproDao = new UserDAO();
+		boolean up = uproDao.updateUser();
 
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
-			// SQL文を準備する
-
-			String sql;
-			PreparedStatement pStmt;
-
-				sql = "update User (name, nickname, post_code, address, tel, birthday, gender, due_date) SET (ムバッペ,俊三,,?,?,?,?,?) WHERE user_id = ?";
-				pStmt= conn.prepareStatement(sql);
-
-				// SQL文を実行する
-				if (pStmt.executeUpdate() == 1) {
-					result = true;
-				}
-			}
-			catch (SQLException e) {
-				e.printStackTrace();
-				result = false;
-			}
-			catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				result = false;
-			}
-		finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-					result = false;
-				}
-			}
-		}
-
-		// 結果を返す
-		return result;
+			System.out.println();
 	}
-
 }
